895f0d4a8d9e8a4a3a5a54a2b8399087f0f36070
levanter914 <shamkashyap25@gmail.com>
2025-04-14 15:54:50 +0530

    basic steup

--- src/app/page.js
+++ src/app/page.js
@@ -5,10 +5,10 @@ export default function Home() {
   return (
     <main className="h-screen flex flex-col items-center justify-center">
       <WavyBackground className="max-w-4xl mx-auto pb-40">
-      <h1 className="text-4xl font-bold mb-6">MoodifyAI 🎧</h1>
+      <h1 className="text-4xl font-bold mb-6 text-white">MoodifyAI 🎧</h1>
       <a
         href="/api/login"
-        className="bg-black text-white px-6 py-3 rounded hover:bg-gray-800"
+        className="bg-white text-black px-6 py-3 rounded hover:bg-gray-300"
       >
         Login with Spotify
       </a>
